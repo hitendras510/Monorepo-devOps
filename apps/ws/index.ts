16cbb165977c44928c55b6a945f6caf529b243a8
Hitendra Singh <hitendrasingh5489@gmail.com>
2026-03-13 12:50:07 +0530

    init

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import {prisma } from "db/client";
+
+Bun.serve({
+  port: 8080,
+  fetch(req,server){
+
+    if(server.upgrade(req)){
+        return;
+    }
+    return new Response("Upgrade failed", {status: 500});
+},
+websocket: {
+    message(ws,message){
+        prisma.user.create({
+            data: {
+                username: Math.random().toString(),
+                password: Math.random().toString()
+            }
+        })
+        ws.send(message);
+    }
+}
+})
